bb0e328711304188587a921b9d9470bc7b8dd0a9
NikolayNIK <nikitin.nikolay@bk.ru>
2018-11-08 10:56:16 +0800

    Unfinished MainActivity

---
+++ app/src/main/java/ru/kollad/forlabs/app/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.kollad.forlabs.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import ru.kollad.forlabs.R;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
