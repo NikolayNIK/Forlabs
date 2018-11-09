4058c56d7d9dbc66e8f720205dedea04f4592ce3
NikolayNIK <nikitin.nikolay@bk.ru>
2018-11-09 17:57:52 +0800

    Dashboard

--- app/src/main/java/ru/kollad/forlabs/app/MainDashboardFragment.java
+++ app/src/main/java/ru/kollad/forlabs/app/MainDashboardFragment.java
@@ -1,13 +1,15 @@
 package ru.kollad.forlabs.app;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import ru.kollad.forlabs.R;
 import ru.kollad.forlabs.model.StudentInfo;
 
@@ -27,10 +29,64 @@ public class MainDashboardFragment extends MainFragment {
 		return fragment;
 	}
 
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.setRetainInstance(false);
+		super.onCreate(savedInstanceState);
+	}
+
 	@Nullable
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.fragment_main_dashboard, container, false);
 	}
 
+	@Override
+	public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+
+		assert getArguments() != null;
+
+		final StudentInfo studentInfo = (StudentInfo) getArguments().getSerializable(KEY_STUDENT_INFO);
+		assert studentInfo != null;
+
+		getToolbar().setTitle(studentInfo.getGroupName());
+
+		final ViewGroup table = view.findViewById(R.id.table_dashboard);
+
+		if(savedInstanceState == null) {
+			Handler handler = new Handler();
+			handler.post(new Runnable() {
+				@Override
+				public void run() {
+					createA(table, studentInfo);
+				}
+			});
+
+			handler.post(new Runnable() {
+				@Override
+				public void run() {
+					createB(table, studentInfo);
+				}
+			});
+		} else {
+			createA(table, studentInfo);
+			createB(table, studentInfo);
+		}
+	}
+
+	private void createA(ViewGroup table, StudentInfo studentInfo) {
+		((TextView) table.findViewById(R.id.text_direction)).setText(studentInfo.getDirectionName());
+		((TextView) table.findViewById(R.id.text_profile)).setText(studentInfo.getProfileName());
+		((TextView) table.findViewById(R.id.text_qualification)).setText(studentInfo.getQualificationName());
+		((TextView) table.findViewById(R.id.text_form)).setText(studentInfo.getStudyFormName());
+	}
+
+	private void createB(ViewGroup table, StudentInfo studentInfo) {
+		((TextView) table.findViewById(R.id.text_year)).setText(getString(R.string.text_dashboard_year_content, studentInfo.getAdmissionYear()));
+		((TextView) table.findViewById(R.id.text_course)).setText(getString(R.string.text_dashboard_course_content, studentInfo.getCurrentCourse()));
+		ProgressBar progressCourse = table.findViewById(R.id.progress_course);
+		progressCourse.setMax(studentInfo.getTotalCourseCount());
+		progressCourse.setProgress(studentInfo.getCurrentCourse());
+	}
 }
