2717ee223a4a4df3abb8fd2a91b831c6f424ae64
NikolayNIK <nikitin.nikolay@bk.ru>
2018-11-12 21:37:53 +0800

    Simplified Studies

--- api/src/main/java/ru/kollad/forlabs/model/Studies.java
+++ api/src/main/java/ru/kollad/forlabs/model/Studies.java
@@ -5,25 +5,18 @@ import org.jsoup.select.Elements;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Provides a list for studies.
  */
-public class Studies implements Serializable {
-	private List<Study> studies;
+public class Studies extends ArrayList<Study> implements Serializable {
 
 	/**
 	 * Constructor for elements.
 	 * @param elements Elements of the page.
 	 */
 	public Studies(Elements elements) {
-		studies = new ArrayList<>();
 		for (Element e : elements)
-			studies.add(new Study(e));
-	}
-
-	public List<Study> getStudies() {
-		return studies;
+			add(new Study(e));
 	}
 }
